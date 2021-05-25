7f165cb8f1be80a405cba53629216f61ffbb0f4d
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-25 19:44:59 +0300

    Update som.py

--- som.py
+++ som.py
@@ -13,14 +13,14 @@ data = np.vstack((age, thalachh)).T
 #iris = datasets.load_iris()
 #iris_data = iris.data[:, :2]
 #iris_label = iris.target
-iris_som = SOM(m=2, n=1, dim=2)
+iris_som = SOM(m=5, n=1, dim=2)
 iris_som.fit(data)
 predictions = iris_som.predict(data)
 # Plot the results
 #fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(5, 7))
 x = data[:, 0]
 y = data[:, 1]
-colors = ['red', 'green']
+colors = ['red', 'green', 'blue', 'purple', 'yellow']
 
 # ax[0].scatter(x, y, cmap=ListedColormap(colors))
 # ax[0].title.set_text('Actual Classes')
