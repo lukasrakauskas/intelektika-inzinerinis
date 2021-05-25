a96bcdfe991121d589300866aec48bb6b71b7563
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-25 20:16:16 +0300

    Finish SOM

--- som.py
+++ som.py
@@ -1,29 +1,109 @@
-from sklearn_som.som import SOM
-from sklearn import datasets
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-import pandas as pd
-import numpy as np
 
+
+class SOM:
+    def __init__(self, m=3, n=3, dim=3, lr=1, sigma=1, max_iter=3000):
+        # Initialize descriptive features of SOM
+        self.m = m
+        self.n = n
+        self.dim = dim
+        self.shape = (m, n)
+        self.initial_lr = lr
+        self.lr = lr
+        self.sigma = sigma
+        self.max_iter = max_iter
+
+        # Initialize weights
+        self.weights = np.random.normal(size=(m * n, dim))
+        self._locations = self._get_locations(m, n)
+
+    def _get_locations(self, m, n):
+        return np.argwhere(np.ones(shape=(m, n))).astype(np.int64)
+
+    def _find_bmu(self, x):
+        # Stack x to have one row per weight
+        x_stack = np.stack([x]*(self.m*self.n), axis=0)
+        # Calculate distance between x and each weight
+        distance = np.linalg.norm(x_stack - self.weights, axis=1)
+        # Find index of best matching unit
+        return np.argmin(distance)
+
+    def step(self, x):
+        # Stack x to have one row per weight
+        x_stack = np.stack([x]*(self.m*self.n), axis=0)
+
+        # Get index of best matching unit
+        bmu_index = self._find_bmu(x)
+
+        # Find location of best matching unit
+        bmu_location = self._locations[bmu_index, :]
+
+        # Find square distance from each weight to the BMU
+        stacked_bmu = np.stack([bmu_location]*(self.m*self.n), axis=0)
+        bmu_distance = np.sum(np.power(self._locations.astype(
+            np.float64) - stacked_bmu.astype(np.float64), 2), axis=1)
+
+        # Compute update neighborhood
+        neighborhood = np.exp((bmu_distance / (self.sigma ** 2)) * -1)
+        local_step = self.lr * neighborhood
+
+        # Stack local step to be proper shape for update
+        local_multiplier = np.stack([local_step]*(self.dim), axis=1)
+
+        # Multiply by difference between input and weights
+        delta = local_multiplier * (x_stack - self.weights)
+
+        # Update weights
+        self.weights += delta
+
+    def fit(self, X, epochs=1, shuffle=True):
+        # Count total number of iterations
+        global_iter_counter = 0
+        n_samples = X.shape[0]
+        total_iterations = np.minimum(epochs * n_samples, self.max_iter)
+
+        for epoch in range(epochs):
+            # Break if past max number of iterations
+            if global_iter_counter > self.max_iter:
+                break
+
+            if shuffle:
+                indices = np.random.permutation(n_samples)
+            else:
+                indices = np.arange(n_samples)
+
+            # Train
+            for idx in indices:
+                # Break if past max number of iterations
+                if global_iter_counter > self.max_iter:
+                    break
+                input = X[idx]
+                # Do one step of training
+                self.step(input)
+                # Update learning rate
+                global_iter_counter += 1
+                self.lr = (1 - (global_iter_counter /
+                                total_iterations)) * self.initial_lr
+
+    def predict(self, X):
+        labels = np.array([self._find_bmu(x) for x in X])
+        return labels
+
+
+som = SOM(m=5, n=1, dim=2)
 df = pd.read_csv('heart.csv')
 age = df['age'].to_numpy()
 thalachh = df['thalachh'].to_numpy()
 data = np.vstack((age, thalachh)).T
-
-#iris = datasets.load_iris()
-#iris_data = iris.data[:, :2]
-#iris_label = iris.target
-iris_som = SOM(m=5, n=1, dim=2)
-iris_som.fit(data)
-predictions = iris_som.predict(data)
-# Plot the results
-#fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(5, 7))
+som.fit(data)
+predictions = som.predict(data)
 x = data[:, 0]
 y = data[:, 1]
 colors = ['red', 'green', 'blue', 'purple', 'yellow']
 
-# ax[0].scatter(x, y, cmap=ListedColormap(colors))
-# ax[0].title.set_text('Actual Classes')
 plt.scatter(x, y, c=predictions, cmap=ListedColormap(colors))
 plt.title('SOM Predictions')
 plt.show()
